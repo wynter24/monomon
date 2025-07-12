6293acfd57de5f6ed13b1ebc400ea5d2c8abab47
wynter24 <m.wynter24.k@gmail.com>
2025-07-12 11:20:18 +0900

    chore: Prettier 설정 업데이트 및 .prettierignore 파일 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,5 +3,8 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'all',
   tabWidth: 2,
+  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
   plugins: ['prettier-plugin-tailwindcss'],
 }; 
