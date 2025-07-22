7073bb4a9ca07120e0cf07293a1e3218bfd22d2b
wynter24 <m.wynter24.k@gmail.com>
2025-07-22 18:56:02 +0900

    feat: 버튼 컴포넌트 및 색상 상수 정의 추가

---
+++ src/constants/button.ts
@@ -0,0 +1,10 @@
+export const BUTTON_SIZE = {
+  md: 'min-w-[245px] px-6',
+  lg: 'min-w-[315px] px-8',
+};
+
+export const VARIANT_COLOR = {
+  inactive: 'bg-[#D9D9D9] hover:bg-gray-light-02',
+  active: 'bg-[#FFDE5B] hover:bg-yellow-darker',
+  disabled: 'bg-gray-light-01 opacity-50 cursor-not-allowed',
+};
