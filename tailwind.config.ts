dde8c6e6ebd79fd68af806d8b9105a931faef350
wynter24 <m.wynter24.k@gmail.com>
2025-07-22 16:58:55 +0900

    feat: autoprefixer 추가 및 Tailwind CSS 설정 업데이트

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    screens: {
-      sm: '375px',
-      md: '744px',
-      lg: '1024px',
-    },
-    extend: {
-      fontFamily: {
-        sans: ['Fredoka', 'ui-sans-serif', 'system-ui'],
-      },
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        gray: {
-          white: '#ffffff',
-          light: '#D9D9D9',
-          darker: '#4B5563',
-          black: '#1F2937',
-        },
-        yellow: {
-          'normal-01': '#FFDE5B',
-        },
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
 
 export default config;
