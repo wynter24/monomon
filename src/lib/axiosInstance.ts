0b9d55e30f2b955ca2eefd2c554ba2f4351d7dc4
wynter24 <m.wynter24.k@gmail.com>
2025-07-23 13:51:42 +0900

    feat: axios 패키지 추가 및 API 호출을 위한 axios 인스턴스 설정, 포켓몬 매칭 기능 구현

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://wynter24-pokemon-face-match.hf.space',
+  timeout: 10000,
+});
+
+// TODO: 로그인 기능 추가 시 interceptor 추가
+// api.interceptors.request.use((config) => {...});
