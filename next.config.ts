d80e05a02a539faebd446911d841cc1c01942705
wynter24 <m.wynter24.k@gmail.com>
2025-07-27 15:51:24 +0900

    feat: 중복 next.config.js 삭제 및 eslint 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 때 lint 스킵
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
