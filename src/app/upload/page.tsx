da144c4c61b17c12e38a48285cb7aa7aca8811a6
wynter24 <m.wynter24.k@gmail.com>
2025-07-30 15:58:48 +0900

    refactor: 로딩 페이지 삭제 및 UI 컴포넌트로 변환 후 upload 페이지 mutation 로딩 처리에 적용용

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,19 +3,39 @@ import { useEffect, useState } from 'react';
 import UploadButton, { showUploadWidget } from '@/components/UploadButton';
 import Button from '@/components/common/Button';
 import UploadPreview from '@/components/UploadPreview';
-import { useMatchStore } from '@/store/useMatchStore';
 import { useRouter } from 'next/navigation';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { matchPokemon } from '@/api/matchPokemon';
+import { savePokemonResult } from '@/api/savePokemonResult';
+import Loading from '@/components/common/Loading';
 
 export default function UploadPage() {
   const [publicId, setPublicId] = useState<string | null>(null);
+  const [uploadedImgUrl, setUploadedImgUrl] = useState<string>('');
+  const [etag, setEtag] = useState<string>('');
   const [isMobile, setIsMobile] = useState(false);
   const router = useRouter();
-  const { setImgUrl, setEtag, clearMatch } = useMatchStore();
 
-  useEffect(() => {
-    clearMatch();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const mutation = useMutation({
+    mutationFn: async ({
+      uploadedImgUrl,
+      etag,
+    }: {
+      uploadedImgUrl: string;
+      etag: string;
+    }) => {
+      const data = await matchPokemon(uploadedImgUrl); // 분석
+      const { id } = await savePokemonResult(etag, data); // 저장
+      return id;
+    },
+    onSuccess: (id) => {
+      router.push(`/result/${id}`);
+    },
+    onError: () => {
+      toast.error('Failed to find a match. Please try again.');
+    },
+  });
 
   useEffect(() => {
     const checkIsMobile = () => {
@@ -30,7 +50,7 @@ export default function UploadPage() {
 
   const handleUploadSuccess = (result: CloudinaryUploadResult) => {
     setPublicId(result.info.public_id);
-    setImgUrl(result.info.secure_url);
+    setUploadedImgUrl(result.info.secure_url);
     setEtag(result.info.etag);
   };
 
@@ -39,36 +59,47 @@ export default function UploadPage() {
   };
 
   return (
-    <div className="container mx-auto flex flex-col gap-11 p-4">
-      <h1 className="text-2xl font-bold">Upload Photo</h1>
+    <>
+      {mutation.isPending ? (
+        <Loading />
+      ) : (
+        <div className="container mx-auto flex flex-col gap-11 p-4">
+          <h1 className="text-2xl font-bold">Upload Photo</h1>
 
-      <div className="flex min-h-[60vh] flex-col items-center justify-center gap-11">
-        <UploadPreview publicId={publicId} onUploadClick={handleUploadClick} />
+          <div className="flex min-h-[60vh] flex-col items-center justify-center gap-11">
+            <UploadPreview
+              publicId={publicId}
+              onUploadClick={handleUploadClick}
+            />
 
-        {/* 버튼 */}
-        <div className="flex max-w-3xs flex-col items-center gap-3">
-          <UploadButton
-            isMobile={isMobile}
-            onUploadSuccess={handleUploadSuccess}
-          />
-          <Button
-            size={isMobile ? 'md' : 'lg'}
-            text="Find my pokemon"
-            disabled={!publicId}
-            variants="active"
-            onClick={() => router.push('/loading')}
-          />
+            {/* 버튼 */}
+            <div className="flex max-w-3xs flex-col items-center gap-3">
+              <UploadButton
+                isMobile={isMobile}
+                onUploadSuccess={handleUploadSuccess}
+              />
+              <Button
+                size={isMobile ? 'md' : 'lg'}
+                text="Find my pokemon"
+                disabled={!publicId}
+                variants="active"
+                onClick={() => mutation.mutate({ uploadedImgUrl, etag })} // mutation 호출
+              />
+            </div>
+          </div>
+
+          <div>
+            <h3 className="mb-3 text-lg font-medium">
+              Tips for the Best Results
+            </h3>
+            <div className="text-gray-darker space-y-2 text-sm">
+              <p>Ensure good lighting on your face.</p>
+              <p>Keep the camera at eye level for better angles.</p>
+              <p>Avoid background distractions.</p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div>
-        <h3 className="mb-3 text-lg font-medium">Tips for the Best Results</h3>
-        <div className="text-gray-darker space-y-2 text-sm">
-          <p>Ensure good lighting on your face.</p>
-          <p>Keep the camera at eye level for better angles.</p>
-          <p>Avoid background distractions.</p>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center">
+        <Image
+          width={70}
+          height={70}
+          className="sm:h-20 sm:w-20"
+          src={'/images/loading_icon.png'}
+          alt="loading"
+        />
+        <div className="mt-4 flex space-x-3 sm:mt-5 md:mt-6">
+          <div className="bg-yellow h-2 w-2 animate-bounce rounded-full sm:h-2.5 sm:w-2.5 md:h-3 md:w-3"></div>
+          <div
+            className="bg-yellow h-2 w-2 animate-bounce rounded-full sm:h-2.5 sm:w-2.5 md:h-3 md:w-3"
+            style={{ animationDelay: '0.1s' }}
+          ></div>
+          <div
+            className="bg-yellow h-2 w-2 animate-bounce rounded-full sm:h-2.5 sm:w-2.5 md:h-3 md:w-3"
+            style={{ animationDelay: '0.2s' }}
+          ></div>
+        </div>
+      </div>
+      <p className="text-lg sm:text-xl md:text-2xl">
+        We’re finding your Pokémon twin
+      </p>
+    </div>
+  );
+}
